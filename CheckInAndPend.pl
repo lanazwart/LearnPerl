18c6c17004d5ad5ae1922147f8695f965acb4051
Lana Zwart <lanazwart@gmail.com>
2014-02-19 16:24:59 +0000

    Non-numeric
    Added non-numeric validation

--- CheckInAndPend.pl
+++ CheckInAndPend.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
-#2014-02-19	Lana Zwart	This script automatically checks in
-#    				and pends .cbl and .uex components in bulk
+#2014-02-19		Lana Zwart		This script automatically checks in
+#    							and pends .cbl and .uex components in bulk
 #                               This was created for the Forward Fit Project	
 #                               SOE088 that requires many components to be
 #                               checked in
@@ -13,20 +13,24 @@ $filename="Input.txt"; #List of components to be checked in - ending in either .
 open MYFILE, $filename or die("Cannot find Input file with list of components to check-in");
 
 while (defined ($line = <MYFILE>)) {
- 
-	if ($line =~ /cbl/i) {
+ 	if ($line =~ /cbl/i) {
 		chdir($proglib) or die("Can't change directory to $proglib\n");
-		chdir(substr($line, 3, 1)) or die("Can't change directory to substr($line, 3, 1)\n") ;
-					
 	}
 	elsif ($line =~ /uex/i) {
 		chdir($uexlib) or die("Can't change directory to $uexlib\n");
-		chdir substr($line, 3, 1) or die("Can't change directory to substr($line, 3, 1)\n") ;
-		
+	}
+
+	$fourth = substr($line, 3, 1); #4th character in component
+	if ($fourth =~ /[0-9]+/) {
+		chdir($fourth) or die("Can't change directory to substr($line, 3, 1)\n") ;
+	}
+	else {
+		chdir(x);
 	}
 	$mydir = `pwd`;
 	print "Changing to $mydir\n";
 	print "Checking in $line\n";
+	
 	$chkout = `cleartool checkin -c "SUE088 - Auto Check In." $line`;
 	if ($chkout) {
 		print "$line checked in successfully\n";
