02f4d6b13f4bd107e0a70c8079e5f44383cdd968
Lana Zwart <lanazwart@gmail.com>
2014-04-23 17:29:41 +0100

    Issue with uexdir
    issue

--- CheckInAndPend.pl
+++ CheckInAndPend.pl
@@ -6,17 +6,33 @@
 #                               SOE088 that requires many components to be
 #                               checked in
 
+# Copy the script and the input file to "Oscar intance"/src/0
+# The input file should contain a list of .cbl and .uex components 
+#     that you'd like to check in and pend automatically.
+# Run the script (perl ChekInAndPend.pl)
+
+
 $proglib="/ellipse/el6.3.3_ry_sup/src/"; #Oscar source
-$uexlib="/ellipse/el6.3.3_tb_sup/copyins/"; #Oscar source
+$uexlib="/ellipse/el6.3.3_tb_sup/copyins/"; #Oscar copyins
 $filename="Input.txt"; #List of components to be checked in - ending in either .cbl or .uex
+$logfile="Logfile.txt";
 
 open MYFILE, $filename or die("Cannot find Input file with list of components to check-in");
+open LOGFILE, ">$logfile" or die("Cannot open log file");
+
+$datetime = scalar(localtime(time));
+
+print LOGFILE "Run Date $datetime\n";
+print LOGFILE "---------------------------\n";
+print LOGFILE "\n";
 
 while (defined ($line = <MYFILE>)) {
  	if ($line =~ /cbl/i) {
+		print "$proglib\n";
 		chdir($proglib) or die("Can't change directory to $proglib\n");
 	}
 	elsif ($line =~ /uex/i) {
+		print "$uexlib\n";
 		chdir($uexlib) or die("Can't change directory to $uexlib\n");
 	}
 
@@ -37,14 +53,18 @@ while (defined ($line = <MYFILE>)) {
 		$pend   = `pending $line`;
 		if ($pend) {
 			print "$line pended successfully\n";
-			}
+			print LOGFILE "$line pended successfully";
+			} 
 		else {
 			print "ERROR with pending of $line\n";
+			print LOGFILE "ERROR $line not pended";
 			}
 		}
 	else { 
 		print "ERROR with check-in of $line\n";
+		print LOGFILE "ERROR with check-in of $line\n";
 	}
 }
-close MYFILE
+close MYFILE;
+close LOGFILE;
 
